12749c4176ddc9e9abc0cfba3e653a37b35f0242
Jet Lin <jetlin101@gmail.com>
2024-11-26 12:21:00 -0800

    made a component

---
+++ src/Components/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainPage() {
+    return (
+      <div className="MainPage">
+        <h1>HackMerced X Live Page</h1>
+      </div>
+    );
+  }
+  
+  export default MainPage;
