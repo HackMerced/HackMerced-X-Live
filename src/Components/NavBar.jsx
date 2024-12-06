e0fed7e6df9fa82584a404cce032406d24de4afa
Jet Lin <jetlin101@gmail.com>
2024-12-05 18:37:15 -0800

    navbar and background

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -7,12 +7,16 @@ function NavBar() {
     return (
       <div className="NavBar">
         <ul className="nav-items">
-        <li><Link to="/"><button className="button" id="home-button">Home</button></Link></li>
-          
+          <img src="/navAssets/navBarLeftPart.svg" className="navbarEnds"/>
+          <li><Link to="/"><button className="button" id="home-button">Home</button></Link></li>
           <li><Link to="/prizes"><button className="button" id="about-us-button">Prizes</button></Link></li>
           <li><Link to="/schedule"><button className="button" id="schedule-button">Schedule</button></Link></li>
           <li><Link to="/punchcard"><button className="button" id="punch-card-button">PunchCard</button></Link></li>
+          
+          <img src="/navAssets/navflair.svg" className="navflair"/>
           <li><Link to="/profile"><button className="button" id="profile-button">Profile</button></Link></li>
+          <img src="/navAssets/navbarRightPart.svg" className="navbarEnds"/>
+          
         </ul>
             
       </div>
