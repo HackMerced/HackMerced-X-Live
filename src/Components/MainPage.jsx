c010a2027117632fa98d518cdf4de700ab059a8c
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-12-27 12:13:48 -0800

    adjust panels

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -4,13 +4,23 @@ import './CSS/MainPage.css'
 function MainPage() {
     return (
       <div className="MainPage">
+        <img src="/mainAssets/HackMerced-Tower.svg" alt="" className="hackmerced-tower" />
         <div className="main-page-contian-panels">
-          <div className="main-page-announcment-panel">
-            <h1>Accouncments</h1>
-          </div>
           <div className="main-page-countdown-panel">
-              <h1>COMPLETE HACKING IN:</h1>
-              <h1>00:00:00</h1>
+              <img src="/mainAssets/Countdown-Background.svg" alt="" className="panel-background" />
+              <div className="panel-text">
+                <h1>COMPLETE HACKING IN:</h1>
+                <h1>00:00:00</h1>
+              </div>
+          </div>
+          <div className="main-page-announcment-panel">
+            <img src="/mainAssets/Prize-Screen.svg" alt="" className="panel-background" />
+            <div className="panel-text">
+                <h1>ANNOUNCMENTS</h1>
+                <div className="contain-announcments">
+                  <p>Snacks available at COB2 105</p>
+                </div>
+            </div>
           </div>
         </div>
       </div>
