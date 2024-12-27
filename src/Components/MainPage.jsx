3165f1a30c3d0f3b33df4d24a38801a04f6eea32
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-12-27 13:10:44 -0800

    add animations

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -8,6 +8,8 @@ function MainPage() {
         <div className="main-page-contian-panels">
           <div className="main-page-countdown-panel">
               <img src="/mainAssets/Countdown-Background.svg" alt="" className="panel-background" />
+              <img src="/mainAssets/purple.svg" alt="" className="purlple-medium-panel" />
+              <img src="/mainAssets/darkpurple.svg" alt="" className="darkpurlple-small-panel" />
               <div className="panel-text">
                 <h1>COMPLETE HACKING IN:</h1>
                 <h1>00:00:00</h1>
