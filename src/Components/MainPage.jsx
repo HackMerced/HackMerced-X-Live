08a8b6e02f7cccf3bc1454b6d6f4f4697a51570e
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-12-26 18:45:31 -0800

    get panels into position

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -4,9 +4,15 @@ import './CSS/MainPage.css'
 function MainPage() {
     return (
       <div className="MainPage">
-
-        <h1>HackMerced X Live Page</h1>
-        <img src="/mainAssets/Group 57.svg" className='event-timer-background' />
+        <div className="main-page-contian-panels">
+          <div className="main-page-announcment-panel">
+            <h1>Accouncments</h1>
+          </div>
+          <div className="main-page-countdown-panel">
+              <h1>COMPLETE HACKING IN:</h1>
+              <h1>00:00:00</h1>
+          </div>
+        </div>
       </div>
     );
   }
