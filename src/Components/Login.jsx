ad0f1bce36971e48aa7abdaf45985a3a3f3a02c5
Aaron Dangc <adraging@gmail.com>
2024-12-30 17:55:52 -0800

    working on login view - got colors in need work on layout and sharp corners

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+// import React from 'react';
 import './CSS/Login.css'
+import { useNavigate } from 'react-router-dom';
 
 
 function Login() {
+  const navigate = useNavigate();
     return (
-      <div className="Login">
-        <h1>HackMerced X Login Page</h1>
-      </div>
+      <>
+        <div className="login-container">
+          <div className="login-box">
+            <h1 className="title">User Login</h1>
+            <form>
+              <input type="email" name="" id="email" placeholder='Email'/>
+              <input type="password" name="" id="pw" placeholder='Password'/>
+              <button type="submit" id='login'>Login</button>
+              <button type="button"
+              id='signup'
+              onClick={() => navigate('/signup')}
+              >New User?</button>
+            </form>
+          </div>
+        </div>
+      </>
+      
     );
   }
   
