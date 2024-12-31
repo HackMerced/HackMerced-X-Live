0f2a570d4bd6bb666e0f84e8f9195476768772b5
Aaron Dangc <adraging@gmail.com>
2024-12-30 19:54:25 -0800

    updated login view

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,14 +1,32 @@
 // import React from 'react';
 import './CSS/Login.css'
 import { useNavigate } from 'react-router-dom';
+// import { useState } from 'react';
 
 
 function Login() {
   const navigate = useNavigate();
+
+
+  // const [text, setText] = useState('');
+  // const [textColor, setTextColor] = useState('black'); // Default text color
+
+  // const handleInputChange = (e) => {
+  //   const value = e.target.value;
+  //   setText(value);
+
+  //   // Change color based on the length of the input or any condition you want
+  //   if (value.length > 5) {
+  //     setTextColor('red');  // Change color to red if input length > 5
+  //   } else {
+  //     setTextColor('black'); // Default color
+  //   }
+  // };
+
+  
     return (
       <>
         <div className="login-container">
-          <div className="login-box">
             <h1 className="title">User Login</h1>
             <form>
               <input type="email" name="" id="email" placeholder='Email'/>
@@ -19,7 +37,6 @@ function Login() {
               onClick={() => navigate('/signup')}
               >New User?</button>
             </form>
-          </div>
         </div>
       </>
       
