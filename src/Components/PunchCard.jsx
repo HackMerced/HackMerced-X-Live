2929b0f69943f2dc0a6e4d93ae3aa7a29a358202
alissonross <alirosssalazar@gmail.com>
2024-12-30 18:04:53 -0800

    More CSS changes to the table contents

--- src/Components/PunchCard.jsx
+++ src/Components/PunchCard.jsx
@@ -10,7 +10,33 @@ function PunchCard() {
           <div className='container-parent'>
             <div className='container-child'>
               <div class="container-child-content">
-
+                <table id="punches" class="rounded-table no-color">
+                  <tr>
+                    <td class="title">Breakfast</td>
+                    <td><div class="punch true"></div></td>
+                    <td><div class="punch false"/></td>
+                    <td><div class="punch false"/></td>
+                    <td class="spacer"></td>
+                  </tr>
+                  <tr>
+                    <td class="title">Lunch</td>
+                    <td><div class="punch true"></div></td>
+                    <td><div class="punch true"></div></td>
+                    <td><div class="punch false"/></td>
+                    <td class="spacer"></td>
+                  </tr>
+                  <tr>
+                    <td class="title">Dinner</td>
+                    <td><div class="punch true"></div></td>
+                    <td><div class="punch true"></div></td>
+                    <td><div class="punch false"/></td>
+                    <td class="spacer"></td>
+                  </tr>
+                  </table>
+                  <center className='punch-inst'>
+                    <p>Your card is automatically punched when you scan at a punch station.</p>
+                  </center>
+                <br/>
               </div>
             </div>
           </div>
