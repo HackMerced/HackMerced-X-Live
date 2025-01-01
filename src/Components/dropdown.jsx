625db8065ef1907fe0385a121b94a47446aa1ad4
Brandon D. <alyssajoseph415@gmail.com>
2025-01-01 11:02:49 -0800

    Update dropdown.jsx

--- src/Components/dropdown.jsx
+++ src/Components/dropdown.jsx
@@ -4,25 +4,13 @@ import { useNavigate } from 'react-router-dom';
 
 function Dropdown() {
   const [isOpen, setIsOpen] = useState(false);
-<<<<<<< HEAD
   const [selectedOption, setSelectedOption] = useState(null);
-  const image = document.createElement("img");
-  image.src = "public\vite.svg";
-
-  const options = [
-    { link: '/profile', label: 'Settings' },
-    { value: '/logout', label: 'Log Out' },
-    { value: '/hackmerced', label: 'HackMerced X' }
-
-    
-=======
   const navigate = useNavigate();
 
   const options = [
     { value: '/profile', label: 'Settings' },
     { value: '/login', label: 'Log Out' },
     // { value: '/hackmerced', label: 'HackMerced X' },
->>>>>>> 27e9616634f22cafd0f0063c2022d0a2d32d870b
   ];
 
   const toggleDropdown = () => {
@@ -37,11 +25,7 @@ function Dropdown() {
   return (
     <div className="dropdown">
       <button className="button" id="profile-button" onClick={toggleDropdown}>
-<<<<<<< HEAD
-        {selectedOption ? selectedOption.src : 'public\vite.svg' }
-=======
         Profile
->>>>>>> 27e9616634f22cafd0f0063c2022d0a2d32d870b
       </button>
       {isOpen && (
         <ul className="dropdown-menu">
