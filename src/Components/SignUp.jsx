1b1612c77a9d7a3660edf1e24f51b5e8a0d24768
Aaron Dangc <adraging@gmail.com>
2025-01-06 16:21:38 -0800

    working on profile page. Need account for responsivesness for prof,login,signup

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,15 +1,10 @@
 
 
 
-import { useState } from "react";
 import "./CSS/SignUp.css";
 
 function SignUp() {
-  const [isAvatarFormVisible, setIsAvatarFormVisible] = useState(false);
-
-  const customizeAvatarClick = () => {
-    setIsAvatarFormVisible(!isAvatarFormVisible);
-  };
+  
 
   return (
     <div className="signup-container">
@@ -27,24 +22,9 @@ function SignUp() {
 
           {/* Profile Section */}
           <div className="profile">
-            <button type="button" onClick={customizeAvatarClick}>
-              Customize Avatar
-            </button>
-
-            {/* Avatar Editor Modal */}
-            {isAvatarFormVisible && (
-              <div className="avatar-customization-modal">
-                <iframe
-                  src="https://beanheads.robertbroersma.com/editor/?accessory=shades&body=chest&circleColor=blue&clothing=tankTop&clothingColor=green&eyebrows=angry&eyes=leftTwitch&faceMask=false&faceMaskColor=white&facialHair=stubble&graphic=redwood&hair=long&hairColor=brown&hat=none5&hatColor=blue&lashes=false&lipColor=purple&mask=true&mouth=serious&skinTone=brown"
-                  width="100%"
-                  height="10%"
-                  frameBorder="0"
-                ></iframe>
-                <button onClick={() => setIsAvatarFormVisible(false)}>
-                  Close Editor
-                </button>
-              </div>
-            )}
+            <input type="file" name="" id="" className="upload"/>
+          
+            
 
             <input type="text" placeholder="Pronouns" />
             <input type="text" placeholder="School" />
@@ -60,67 +40,3 @@ function SignUp() {
 }
 
 export default SignUp;
-
-
-// import React, { useState } from 'react';
-
-// function SignUp() {
-//   const [avatarUrl, setAvatarUrl] = useState('');
-//   const [isEditorOpen, setIsEditorOpen] = useState(false);
-
-//   // Function to open the Beanhead editor
-//   const openEditor = () => {
-//     // Open the Beanhead editor in a new window or iframe
-//     const editorWindow = window.open(
-//       'https://beanheads.robertbroersma.com/editor',
-//       '_blank',
-//       'width=800,height=600'
-//     );
-
-//     // You can listen for events or trigger the download link after customization
-//     editorWindow.addEventListener('message', (event) => {
-//       if (event.origin === 'https://beanheads.robertbroersma.com') {
-//         const svgData = event.data;  // Assuming editor sends SVG data (or URL)
-//         // Convert SVG data to a Data URL (Base64-encoded)
-//         const dataUrl = `data:image/svg+xml;base64,${btoa(svgData)}`;
-//         setAvatarUrl(dataUrl); // Save the Data URL (or URL if available)
-//         console.log('Avatar URL:', dataUrl);
-//       }
-//     });
-//   };
-
-//   const handleFormSubmit = (e) => {
-//     e.preventDefault();
-//     // Here you can submit the avatar URL to your backend and store it in the database
-//     console.log('Avatar URL:', avatarUrl);
-
-//     // Example: submit form data with avatarUrl
-//     // axios.post('/api/register', { avatar: avatarUrl, ...formData });
-//   };
-
-//   return (
-//     <div className="signup-container">
-//       <h1>User Registration</h1>
-//       <form onSubmit={handleFormSubmit}>
-//         {/* Other form fields */}
-
-//         <button type="button" onClick={openEditor}>
-//           Customize Avatar
-//         </button>
-
-//         {/* Display the avatar URL */}
-//         <input
-//           type="text"
-//           placeholder="Avatar URL"
-//           value={avatarUrl}
-//           disabled
-//         />
-
-//         {/* Submit button */}
-//         <button type="submit">Register</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default SignUp;
