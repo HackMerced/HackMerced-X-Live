38bbb1dfd7b99c230d73c137b975dec621a5c45e
Jet Lin <jetlin101@gmail.com>
2025-01-12 16:25:11 -0800

    jet schedule fix for super long contnet

--- src/Components/Schedule.jsx
+++ src/Components/Schedule.jsx
@@ -19,13 +19,15 @@ function Schedule() {
           <img src="/scheduleAssets/SmallSchedulePanel.png" className='small-schedule-panel' alt='Schedule Panel'/>
           <img src="/scheduleAssets/SchedulePanel.png" className='schedule-panel' alt='Schedule Panel'/>
           <div className='event-content'>
-            <p className='event-title'><b>Event Title</b></p>
-            <p className='start-time'><b>Start: </b>10:00am</p>
-            <p className='duration'><b>Duration: </b>1 hr</p>
-            <p className='location'><b>Location: </b>COB2 130</p>
-            <p className='contents'>learn the basics of firebase.  Implement login, logout, and sign up capabilities.  Create a live update comment section using firestore.  All experiance levels welcome!</p>
-            <p className='capacity'><b>Capacity: </b>20</p>
-            <p className='reward'><b>Reward: </b>100 points</p>
+            <div className='event-text'>
+              <p className='event-title'><b>Event Title</b></p>
+              <p className='start-time'><b>Start: </b>10:00am</p>
+              <p className='duration'><b>Duration: </b>1 hr</p>
+              <p className='location'><b>Location: </b>COB2 130</p>
+              <p className='contents'>learn the basics of firebase.  Implement login, logout, and sign up capabilities.  Create a live update comment section using firestore.  All experiance levels welcome!</p>
+              <p className='capacity'><b>Capacity: </b>20</p>
+              <p className='reward'><b>Reward: </b>100 points</p>
+            </div>
             <p><img src="/scheduleAssets/maps/COB2130.png" className='map' alt='COB2 130'/></p>
           </div>
         </div>
@@ -34,13 +36,15 @@ function Schedule() {
           <img src="/scheduleAssets/SmallSchedulePanel.png" className='small-schedule-panel' alt='Schedule Panel'/>
           <img src="/scheduleAssets/SchedulePanel.png" className='schedule-panel' alt='Schedule Panel'/>
           <div className='event-content'>
-            <p className='event-title'><b>I Figma'd it out! Workshop</b></p>
-            <p className='start-time'><b>Start: </b>10:00am</p>
-            <p className='duration'><b>Duration: </b>1 hr</p>
-            <p className='location'><b>Location: </b>COB2 130</p>
-            <p className='contents'>Learn how to use figma to create designs!</p>
-            <p className='capacity'><b>Capacity: </b>20</p>
-            <p className='reward'><b>Reward: </b>100 points</p>
+            <div className='event-text'>
+              <p className='event-title'><b>I Figma'd it out! Workshop</b></p>
+              <p className='start-time'><b>Start: </b>10:00am</p>
+              <p className='duration'><b>Duration: </b>1 hr</p>
+              <p className='location'><b>Location: </b>COB2 130</p>
+              <p className='contents'>Learn how to use figma to create designs!</p>
+              <p className='capacity'><b>Capacity: </b>20</p>
+              <p className='reward'><b>Reward: </b>100 points</p>
+            </div>
             <img src="/scheduleAssets/maps/COB2130.png" className='map' alt='COB2 130'/>
           </div>
         </div>
@@ -49,13 +53,15 @@ function Schedule() {
           <img src="/scheduleAssets/SmallSchedulePanel.png" className='small-schedule-panel' alt='Schedule Panel'/>
           <img src="/scheduleAssets/SchedulePanel.png" className='schedule-panel' alt='Schedule Panel'/>
           <div className='event-content'>
-            <p className='event-title'><b>FireBase WorkShop</b></p>
-            <p className='start-time'><b>Start: </b>10:00am</p>
-            <p className='duration'><b>Duration: </b>1 hr</p>
-            <p className='location'><b>Location: </b>COB2 130</p>
-            <p className='contents'>learn the basics of firebase.  Implement login, logout, and sign up capabilities.  Create a live update comment section using firestore.  All experiance levels welcome!</p>
-            <p className='capacity'><b>Capacity: </b>20</p>
-            <p className='reward'><b>Reward: </b>100 points</p>
+            <div className='event-text'>
+              <p className='event-title'><b>FireBase WorkShop</b></p>
+              <p className='start-time'><b>Start: </b>10:00am</p>
+              <p className='duration'><b>Duration: </b>1 hr</p>
+              <p className='location'><b>Location: </b>COB2 130</p>
+              <p className='contents'>learn the basics of firebase.  Implement login, logout, and sign up capabilities.  Create a live update comment section using firestore.  All experiance levels welcome!</p>
+              <p className='capacity'><b>Capacity: </b>20</p>
+              <p className='reward'><b>Reward: </b>100 points</p>
+            </div>
             <img src="/scheduleAssets/maps/COB2130.png" className='map' alt='COB2 130'/>
           </div>
         </div>
@@ -64,13 +70,15 @@ function Schedule() {
           <img src="/scheduleAssets/SmallSchedulePanel.png" className='small-schedule-panel' alt='Schedule Panel'/>
           <img src="/scheduleAssets/SchedulePanel.png" className='schedule-panel' alt='Schedule Panel'/>
           <div className='event-content'>
-            <p className='event-title'><b>Chat GPT From Scratch Workshop</b></p>
-            <p className='start-time'><b>Start: </b>10:00am</p>
-            <p className='duration'><b>Duration: </b>1 hr</p>
-            <p className='location'><b>Location: </b>COB2 130</p>
-            <p className='contents'>Learn to pretrain a LLM.  We will go over self attention, transformer architechure, cool matrix tricks and more.  All you need to know is basic python syntax! No experiance needed!</p>
-            <p className='capacity'><b>Capacity: </b>20</p>
-            <p className='reward'><b>Reward: </b>100 points</p>
+            <div className='event-text'>
+              <p className='event-title'><b>Chat GPT From Scratch Workshop</b></p>
+              <p className='start-time'><b>Start: </b>10:00am</p>
+              <p className='duration'><b>Duration: </b>1 hr</p>
+              <p className='location'><b>Location: </b>COB2 130</p>
+              <p className='contents'>Learn to pretrain a LLM.  We will go over self attention, transformer architechure, cool matrix tricks and more.  All you need to know is basic python syntax! No experiance needed!</p>
+              <p className='capacity'><b>Capacity: </b>20</p>
+              <p className='reward'><b>Reward: </b>100 points</p>
+            </div>
             <img src="/scheduleAssets/maps/COB2130.png" className='map' alt='COB2 130'/>
           </div>
         </div>
@@ -79,13 +87,15 @@ function Schedule() {
           <img src="/scheduleAssets/SmallSchedulePanel.png" className='small-schedule-panel' alt='Schedule Panel'/>
           <img src="/scheduleAssets/SchedulePanel.png" className='schedule-panel' alt='Schedule Panel'/>
           <div className='event-content'>
-            <p className='event-title'><b>Blender Workshop</b></p>
-            <p className='start-time'><b>Start: </b>10:00am</p>
-            <p className='duration'><b>Duration: </b>1 hr</p>
-            <p className='location'><b>Location: </b>COB2 130</p>
-            <p className='contents'>Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5</p>
-            <p className='capacity'><b>Capacity: </b>20</p>
-            <p className='reward'><b>Reward: </b>100 points</p>
+            <div className='event-text'>
+              <p className='event-title'><b>Blender Workshop</b></p>
+              <p className='start-time'><b>Start: </b>10:00am</p>
+              <p className='duration'><b>Duration: </b>1 hr</p>
+              <p className='location'><b>Location: </b>COB2 130</p>
+              <p className='contents'>Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5</p>
+              <p className='capacity'><b>Capacity: </b>20</p>
+              <p className='reward'><b>Reward: </b>100 points</p>
+            </div>
             <img src="/scheduleAssets/maps/COB2130.png" className='map' alt='COB2 130'/>
           </div>
         </div>
@@ -94,13 +104,15 @@ function Schedule() {
           <img src="/scheduleAssets/SmallSchedulePanel.png" className='small-schedule-panel' alt='Schedule Panel'/>
           <img src="/scheduleAssets/SchedulePanel.png" className='schedule-panel' alt='Schedule Panel'/>
           <div className='event-content'>
-            <p className='event-title'><b>Blender Workshop</b></p>
-            <p className='start-time'><b>Start: </b>10:00am</p>
-            <p className='duration'><b>Duration: </b>1 hr</p>
-            <p className='location'><b>Location: </b>COB2 130</p>
-            <p className='contents'>Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5</p>
-            <p className='capacity'><b>Capacity: </b>20</p>
-            <p className='reward'><b>Reward: </b>100 points</p>
+            <div className='event-text'>
+              <p className='event-title'><b>Blender Workshop</b></p>
+              <p className='start-time'><b>Start: </b>10:00am</p>
+              <p className='duration'><b>Duration: </b>1 hr</p>
+              <p className='location'><b>Location: </b>COB2 130</p>
+              <p className='contents'>Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5</p>
+              <p className='capacity'><b>Capacity: </b>20</p>
+              <p className='reward'><b>Reward: </b>100 points</p>
+            </div>
             <img src="/scheduleAssets/maps/COB2130.png" className='map' alt='COB2 130'/>
           </div>
         </div>
@@ -109,13 +121,15 @@ function Schedule() {
           <img src="/scheduleAssets/SmallSchedulePanel.png" className='small-schedule-panel' alt='Schedule Panel'/>
           <img src="/scheduleAssets/SchedulePanel.png" className='schedule-panel' alt='Schedule Panel'/>
           <div className='event-content'>
-            <p className='event-title'><b>Blender Workshop</b></p>
-            <p className='start-time'><b>Start: </b>10:00am</p>
-            <p className='duration'><b>Duration: </b>1 hr</p>
-            <p className='location'><b>Location: </b>COB2 130</p>
-            <p className='contents'>Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5</p>
-            <p className='capacity'><b>Capacity: </b>20</p>
-            <p className='reward'><b>Reward: </b>100 points</p>
+            <div className='event-text'>
+              <p className='event-title'><b>Blender Workshop</b></p>
+              <p className='start-time'><b>Start: </b>10:00am</p>
+              <p className='duration'><b>Duration: </b>1 hr</p>
+              <p className='location'><b>Location: </b>COB2 130</p>
+              <p className='contents'>Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5</p>
+              <p className='capacity'><b>Capacity: </b>20</p>
+              <p className='reward'><b>Reward: </b>100 points</p>
+            </div>
             <img src="/scheduleAssets/maps/COB2130.png" className='map' alt='COB2 130'/>
           </div>
         </div>
@@ -124,13 +138,15 @@ function Schedule() {
           <img src="/scheduleAssets/SmallSchedulePanel.png" className='small-schedule-panel' alt='Schedule Panel'/>
           <img src="/scheduleAssets/SchedulePanel.png" className='schedule-panel' alt='Schedule Panel'/>
           <div className='event-content'>
-            <p className='event-title'><b>Blender Workshop</b></p>
-            <p className='start-time'><b>Start: </b>10:00am</p>
-            <p className='duration'><b>Duration: </b>1 hr</p>
-            <p className='location'><b>Location: </b>COB2 130</p>
-            <p className='contents'>Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5</p>
-            <p className='capacity'><b>Capacity: </b>20</p>
-            <p className='reward'><b>Reward: </b>100 points</p>
+            <div className='event-text'>
+              <p className='event-title'><b>Blender Workshop</b></p>
+              <p className='start-time'><b>Start: </b>10:00am</p>
+              <p className='duration'><b>Duration: </b>1 hr</p>
+              <p className='location'><b>Location: </b>COB2 130</p>
+              <p className='contents'>Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5</p>
+              <p className='capacity'><b>Capacity: </b>20</p>
+              <p className='reward'><b>Reward: </b>100 points</p>
+            </div>
             <img src="/scheduleAssets/maps/COB2130.png" className='map' alt='COB2 130'/>
           </div>
         </div>
@@ -139,13 +155,15 @@ function Schedule() {
           <img src="/scheduleAssets/SmallSchedulePanel.png" className='small-schedule-panel' alt='Schedule Panel'/>
           <img src="/scheduleAssets/SchedulePanel.png" className='schedule-panel' alt='Schedule Panel'/>
           <div className='event-content'>
-            <p className='event-title'><b>Blender Workshop</b></p>
-            <p className='start-time'><b>Start: </b>10:00am</p>
-            <p className='duration'><b>Duration: </b>1 hr</p>
-            <p className='location'><b>Location: </b>COB2 130</p>
-            <p className='contents'>Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5</p>
-            <p className='capacity'><b>Capacity: </b>20</p>
-            <p className='reward'><b>Reward: </b>100 points</p>
+            <div className='event-text'>
+              <p className='event-title'><b>Blender Workshop</b></p>
+              <p className='start-time'><b>Start: </b>10:00am</p>
+              <p className='duration'><b>Duration: </b>1 hr</p>
+              <p className='location'><b>Location: </b>COB2 130</p>
+              <p className='contents'>Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5</p>
+              <p className='capacity'><b>Capacity: </b>20</p>
+              <p className='reward'><b>Reward: </b>100 points</p>
+            </div>
             <img src="/scheduleAssets/maps/COB2130.png" className='map' alt='COB2 130'/>
           </div>
         </div>
@@ -154,13 +172,15 @@ function Schedule() {
           <img src="/scheduleAssets/SmallSchedulePanel.png" className='small-schedule-panel' alt='Schedule Panel'/>
           <img src="/scheduleAssets/SchedulePanel.png" className='schedule-panel' alt='Schedule Panel'/>
           <div className='event-content'>
-            <p className='event-title'><b>Blender Workshop</b></p>
-            <p className='start-time'><b>Start: </b>10:00am</p>
-            <p className='duration'><b>Duration: </b>1 hr</p>
-            <p className='location'><b>Location: </b>COB2 130</p>
-            <p className='contents'>Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5</p>
-            <p className='capacity'><b>Capacity: </b>20</p>
-            <p className='reward'><b>Reward: </b>100 points</p>
+            <div className='event-text'>
+              <p className='event-title'><b>Blender Workshop</b></p>
+              <p className='start-time'><b>Start: </b>10:00am</p>
+              <p className='duration'><b>Duration: </b>1 hr</p>
+              <p className='location'><b>Location: </b>COB2 130</p>
+              <p className='contents'>Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5Make a 3d donut!  We will be following Blender Guru's tutorial.  Note that this workshop uses Blender 3.5</p>
+              <p className='capacity'><b>Capacity: </b>20</p>
+              <p className='reward'><b>Reward: </b>100 points</p>
+            </div>
             <img src="/scheduleAssets/maps/COB2130.png" className='map' alt='COB2 130'/>
           </div>
         </div>
